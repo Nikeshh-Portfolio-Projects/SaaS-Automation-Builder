6fdb29bfc16cda7f2edb452d527a717829e0353f
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-08 21:33:12 -0400

    Null handling

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -22,7 +22,7 @@ const UploadCareButton = ({ onUpload }: Props) => {
         router.refresh()
       }
     }
-    ctxProviderRef.current.addEventListener('file-upload-success', handleUpload)
+    ctxProviderRef?.current?.addEventListener('file-upload-success', handleUpload)
   }, [])
 
   return (
